bc7cd38e8f26482c61774e560b6af0fe7c2b8a98
Michael Rafferty <103801065+Michael-Rafferty-Macys@users.noreply.github.com>
2022-07-19 16:51:01 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  Link,
+  Route,
+  Switch,
+  useLocation,
+} from "react-router-dom";
+
+function Home() {
+  return <h1>Welcome to the home page</h1>;
+}
+
+function About() {
+  return <h1>You are on the about page</h1>;
+}
+
+function Contact() {
+  return <h1>Please feel free to email us</h1>;
+}
+
+function NoMatch() {
+  const location = useLocation();
+  return <h1>404 Not Found: <code>{location.pathname}</code></h1>;
+}
+function App () {
+  return (
+      <div className="App">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Switch>
+        
+          <Route exact path="/">
+            <Home />
+          </Route>
+          
+          <Route path="/About">
+            <About />
+          </Route>
+        
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        
+          <Route>
+            <NoMatch />
+          </Route>
+        
+        </Switch>
+      </div>
+  );
+}
+
+export default App;
